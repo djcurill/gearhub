1351c54c0758487b84e6b890fe368c00dfd7b02e
Dan Curilla <curilladaniel@gmail.com>
2021-10-24 22:40:49 -0600

    Connect to mongodb

--- src/config/options.js
+++ src/config/options.js
@@ -1,7 +1,15 @@
-const env = process.env.NODE_ENV || 'development';
+const dbUtils = require('../utils/db');
 
+const env = process.env.NODE_ENV || 'development';
 const config = require('dotenv').config({
   path: `./.env.${env}`,
 });
 
-// module.exports = { options };
+const dbConnectionString = dbUtils.buildConnectionString(
+  'mongodb',
+  process.env.DB_HOST,
+  process.env.DB_PORT,
+  process.env.DB_NAME
+);
+
+module.exports = { dbConnectionString: dbConnectionString };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const config = require('./config/options.js').options;
+const config = require('./config/options.js');
+const dbUtils = require('./utils/db');
 
 const port = process.env.PORT;
 
 const app = express();
+
+const cnxn = dbUtils.connect(config.dbConnectionString);
 
 app.get('/', function (req, res) {
   res.send('Hello World');
