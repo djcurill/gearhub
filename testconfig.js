dd7052122129eed72a4e79d77f1094c976edb45c
Dan Curilla <curilladaniel@gmail.com>
2021-10-28 06:48:36 -0600

    next step: wipe database after each test

--- testconfig.js
+++ testconfig.js
@@ -3,16 +3,25 @@ const cuid = require('cuid');
 const { connect, removeAllCollections } = require('./src/utils/db');
 const url = 'mongodb://localhost:27017/testdb-';
 
-beforeAll(async () => {
-  await connect(url + cuid());
-});
+const DISCONNECTED = 0;
 
+// Before each tests:
+// If db is is disconnected: Connect, clear db, catch error
+// otherwise: just clear the db
 beforeEach(async (done) => {
   const db = cuid();
-
+  // code goes here
   function clearDb() {}
 });
 
-afterEach(async () => {
-  await removeAllCollections();
+// after every test, drop database and disconnect
+afterEach((done) => {
+  mongoose.connection.db.dropDatabase();
+  mongoose.disconnect();
+  return done();
+});
+
+// notify jest we are done all tests
+afterAll((done) => {
+  return done();
 });
